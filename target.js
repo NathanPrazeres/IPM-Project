7d853b817f242f1c64941342da467cdfd069bbfa
NathanPrazeres <90866687+NathanPrazeres@users.noreply.github.com>
2023-03-29 16:17:10 +0100

    Leis em comment + More recognizable big letter

--- target.js
+++ target.js
@@ -25,8 +25,9 @@ class Target
     // Draw target
     fill(bg_color);                 
     circle(this.x, this.y, this.width);
+    // Draw black inner circle to contrast with text
     fill(color(0, 0, 0));
-    circle(this.x, this.y, this.width - 12);
+    circle(this.x, this.y, this.width * 0.85);
     
     // Draw label
     textFont("Arial", 12);
@@ -34,14 +35,24 @@ class Target
     textAlign(CENTER);
     text(this.label, this.x, this.y);
 
+    // Law of proximity: draw a box around the target
+    // Law of common region: draw a box around the target
+    // Law of similarity: draw a box around the target
+    // Law of connectedness: draw a box around the target
+    // Miller's Law: Only have less than 7 colors in the design
+    // Hick's Law: don't divide the targets into categories because there would 2 searches necessary
+
     // Draw boxes around same letter targets and a big letter before each new letter
     if (first)
     {
       textFont("Arial", 36);
       fill(color(255, 255, 0));
       textAlign(CENTER);
-      text(this.label[0], this.x - this.width/2 - 20, this.y + this.width/8);
+      stroke(255, 0, 0);
+      strokeWeight(4);
+      text(this.label[0], this.x - this.width * 3/4, this.y + this.width/8);
 
+      strokeWeight(1);
       noFill();
       stroke(255);
       // quarter circle at the top of the target
@@ -49,14 +60,14 @@ class Target
       // quarter circle at the bottom of the target
       arc(this.x - this.width/2, this.y + this.width/4, this.width/2, this.width/2, PI / 2, PI);
       if (last) {
-        // lines to connect the four quarter circles in the case of a single target
-        line(this.x - this.width/2, this.y - this.width/2, this.x + this.width/2 - 20, this.y - this.width/2);
-        line(this.x - this.width/2, this.y + this.width/2, this.x + this.width/2 - 20, this.y + this.width/2);
+        // lines to connect the four quarter circles (two by two) in the case of a single target
+        line(this.x - this.width/2, this.y - this.width/2, this.x + this.width/4, this.y - this.width/2);
+        line(this.x - this.width/2, this.y + this.width/2, this.x + this.width/4, this.y + this.width/2);
       }
       else {
         // lines to connect the two quarter circles to the next target
-        line(this.x - this.width/2, this.y - this.width/2, this.x + this.width/2 + 28, this.y - this.width/2);
-        line(this.x - this.width/2, this.y + this.width/2, this.x + this.width/2 + 28, this.y + this.width/2);
+        line(this.x - this.width/2, this.y - this.width/2, this.x + this.width/4 + this.width/2, this.y - this.width/2);
+        line(this.x - this.width/2, this.y + this.width/2, this.x + this.width/4 + this.width/2, this.y + this.width/2);
       }
     }
     if (last)
@@ -68,13 +79,12 @@ class Target
       // quarter circle at the bottom of the target
       arc(this.x + this.width/4, this.y - this.width/4, this.width/2, this.width/2, PI * 3/2, 0);
 
-      textAlign(CENTER);
       // line connecting the two quarter circles
       line(this.x + this.width/2, this.y - this.width/4, this.x + this.width/2, this.y + this.width/4);
       if (!first) {
         // lines above and below the target to connect it to the previous target
-        line(this.x - this.width/2 - 32, this.y - this.width/2, this.x + this.width/2 - 20, this.y - this.width/2);
-        line(this.x - this.width/2 - 32, this.y + this.width/2, this.x + this.width/2 - 20, this.y + this.width/2);
+        line(this.x - this.width, this.y - this.width/2, this.x + this.width/4, this.y - this.width/2);
+        line(this.x - this.width, this.y + this.width/2, this.x + this.width/4, this.y + this.width/2);
       }
     }
     if (!first && !last) {
@@ -82,8 +92,8 @@ class Target
       stroke(255);
 
       // lines to connect to both the next and previous targets
-      line(this.x - this.width + 12, this.y - this.width/2, this.x + this.width - 20, this.y - this.width/2);
-      line(this.x - this.width + 12, this.y + this.width/2, this.x + this.width - 20, this.y + this.width/2);
+      line(this.x - this.width, this.y - this.width/2, this.x + this.width - this.width/4, this.y - this.width/2);
+      line(this.x - this.width, this.y + this.width/2, this.x + this.width - this.width/4, this.y + this.width/2);
     }
     noStroke();
 
